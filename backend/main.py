f64685610793c94277a9545aa16c79414b14826c
joshb-code <joshuabsebastian@gmail.com>
2026-06-05 16:08:28 -0400

    Initialize database connection in main application

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from backend.routes import listings, upload
+from db.database import init_db
+from routes import listings, upload
 
 app  = FastAPI(title = "Ai Marketplace")
+init_db()
 
 app.include_router(listings.router, prefix="/listings")
 app.include_router(upload.router, prefix="/upload")
