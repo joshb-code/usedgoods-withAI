8dea5e27c2d42ac2a34251d0e285258f93424ce9
joshb-code <joshuabsebastian@gmail.com>
2026-06-09 15:58:51 -0400

    Refactor listings retrieval and remove unused create_listing endpoint

--- backend/routes/listings.py
+++ backend/routes/listings.py
@@ -2,14 +2,10 @@ from fastapi import APIRouter, Depends
 from db.database import get_db
 router = APIRouter()
 
-@router.post("/listings")
-async def create_listing(title: str, description:str, price: float, db = Depends(get_db)):
-    db.execute("INSERT INTO listings (title, description, price) VALUES (?, ?, ?)", (title, description, price))
-    return db.execute("SELECT last_insert_rowid()").fetchone()[0]
 
 #returns all listings in the database
 @router.get("/listings")
 async def get_listings(db = Depends(get_db)):
-    db.execute("SELECT * FROM listings")
-    listings = db.fetchall()
+    listings = db.execute("SELECT * FROM listings")
+    listings = listings.fetchall()
     return listings
